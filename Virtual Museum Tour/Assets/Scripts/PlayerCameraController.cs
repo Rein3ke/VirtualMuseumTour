e85daaef38d2fd387defc528f051dcb8a8fef93b
Marvin Kullick <mkullick@web.de>
2021-12-09 21:23:03 +0100

    Add PlayerCameraController; Ad UI to tags; Add navigation user interface prefab;

---
+++ Virtual Museum Tour/Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+
+    private void Start()
+    {
+        // When spawning player prefab, iterate through all cameras and deactivate everyone which isn't the player camera.
+        foreach (var currentCamera in FindObjectsOfType<Camera>())
+        {
+            currentCamera.enabled = currentCamera.Equals(playerCamera);
+        }
+    }
+}
