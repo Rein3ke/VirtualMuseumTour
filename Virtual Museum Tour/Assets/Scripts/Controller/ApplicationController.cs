7abc4608d196e6282e62ef8ff771532ff37bd0c2
Marvin Kullick <mkullick@web.de>
2021-12-19 15:20:08 +0100

    Move ApplicationController.cs to controller folder;

---
+++ Virtual Museum Tour/Assets/Scripts/Controller/ApplicationController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+// Singleton
+namespace Controller
+{
+    public class ApplicationController : MonoBehaviour
+    {
+        public static ApplicationController Instance { get; private set; }
+    
+        private GameObject _playerSpawnControllerGameObject;
+        private GameObject _exhibitManagerGameObject;
+
+        private const string PrefabPath = "prefabs";
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+    
+        private void Start()
+        {
+            ControllerSetup();
+        }
+
+        private void ControllerSetup()
+        {
+            _playerSpawnControllerGameObject = Instantiate(LoadFromResourcesAsGameObject("PlayerSpawnController"), transform);
+            _exhibitManagerGameObject = Instantiate(LoadFromResourcesAsGameObject("ExhibitManager"), transform);
+        }
+
+        private GameObject GetManagerGameObject()
+        {
+            var manager = GameObject.FindGameObjectWithTag("GameController");
+
+            if (manager == null)
+            {
+                Debug.LogError("Can't find Game Controller game object in scene.");
+                return null;
+            }
+            else
+            {
+                return manager;
+            }
+        }
+
+        private static GameObject LoadFromResourcesAsGameObject(string prefab)
+        {
+            return Resources.Load($"{PrefabPath}/{prefab}") as GameObject;
+        }
+    }
+}
