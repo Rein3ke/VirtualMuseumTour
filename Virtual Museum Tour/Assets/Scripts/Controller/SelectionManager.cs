4f541fa9f2fa6b766174aec2181957aabce8c641
Marvin Kullick <mkullick@web.de>
2021-12-22 13:16:37 +0100

    Enable selection via raycast by SelectionManager.cs (Singleton);

--- Virtual Museum Tour/Assets/Scripts/Controller/SelectionManager.cs
+++ Virtual Museum Tour/Assets/Scripts/Controller/SelectionManager.cs
@@ -1,20 +1,38 @@
+using System;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Controller
 {
     public class SelectionManager : MonoBehaviour
     {
+        public static SelectionManager Instance { get; private set; }
+        
         [SerializeField] private Material highlightMaterial;
+
+        public event EventHandler<OnExhibitSelectedEventArgs> OnExhibitSelected;
 
         private Transform _selection;
         private Material _selectionMaterial;
-        
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+
         private void Update()
         {
             // 1. check for main camera
             var mainCamera = Camera.main;
             if (mainCamera == null) return;
-            
+
             // 2. if something is selected, reset
             if (_selection != null)
             {
@@ -41,14 +59,64 @@ namespace Controller
 
                     _selection = selection;
                 }
-                
             }
 
             // (5.) do something if something is selected and left mouse button is pressed
             if (Input.GetMouseButtonDown(0) && _selection != null)
             {
-                Debug.Log($"Selected: {_selection.name}");
+                // Find anchor of selection and invoke event
+                if (HasExhibitAnchor(_selection.gameObject, out var anchor))
+                {
+                    
+                    ExhibitManager.Instance.ExhibitDictionary.TryGetValue(anchor.GetComponent<ExhibitAnchor>().ExhibitID, out var exhibit);
+
+                    InvokeOnExhibitSelected(exhibit);
+                }
             }
+        }
+
+        private bool HasExhibitAnchor(GameObject selection, out GameObject anchor)
+        {
+            GameObject current = selection;
+            
+            do
+            {
+                if (current.CompareTag("ExhibitAnchor"))
+                {
+                    anchor = current;
+                    return true;
+                }
+
+                current = current.transform.parent.gameObject;
+                
+            } while (current.transform.parent != null);
+
+            anchor = null;
+            return false;
+        }
+        
+        #region Events
+
+        private void InvokeOnExhibitSelected(Exhibit exhibit)
+        {
+            if (exhibit == null)
+            {
+                Debug.LogError("Given exhibit is null!", this);
+                return;
+            }
+            OnExhibitSelected?.Invoke(this, new OnExhibitSelectedEventArgs(exhibit));
+        }
+
+        #endregion
+    }
+
+    public class OnExhibitSelectedEventArgs: EventArgs
+    {
+        public Exhibit Exhibit { get; private set; }
+
+        public OnExhibitSelectedEventArgs(Exhibit exhibit)
+        {
+            Exhibit = exhibit;
         }
     }
 }
