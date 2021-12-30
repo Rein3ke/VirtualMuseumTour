807c1fa7590a860967adb9aa5c56104d0384cb2e
Marvin Kullick <mkullick@web.de>
2021-12-30 20:31:07 +0100

    Move class OnVisibilityChangeEventArgs.cs to its own file;

---
+++ Virtual Museum Tour/Assets/Scripts/Interface/OnVisibilityChangeEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Interface
+{
+    public class OnVisibilityChangeEventArgs : EventArgs
+    {
+        public bool IsVisible { get; private set; }
+
+        public OnVisibilityChangeEventArgs(bool isVisible)
+        {
+            IsVisible = isVisible;
+        }
+    }
+}
