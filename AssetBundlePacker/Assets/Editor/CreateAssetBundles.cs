cf8c8f47846fb79d1b63ad6b448e218e1dda5580
Marvin Kullick <mkullick@web.de>
2022-01-13 18:00:53 +0100

    Redone CreateAssetBundle script;

--- AssetBundlePacker/Assets/Editor/CreateAssetBundles.cs
+++ AssetBundlePacker/Assets/Editor/CreateAssetBundles.cs
@@ -1,21 +1,54 @@
 using System.IO;
 using UnityEditor;
+using UnityEngine;
 
 namespace Editor
 {
     public static class CreateAssetBundles
     {
-        [MenuItem("Assets/Build AssetBundles")]
-        private static void BuildAllAssetBundles()
+        private const string AssetBundleDirectory = "Assets/StreamingAssets";
+
+        private static readonly BuildTarget[] SupportedBuildTargets =
         {
-            var assetBundleDirectory = "Assets/StreamingAssets";
+            BuildTarget.StandaloneWindows,
+            BuildTarget.WebGL,
+            BuildTarget.NoTarget
+        };
+
+        private static void BuildAssetBundle(BuildTarget target)
+        {
+            Debug.LogWarning($"Building AssetBundles for target: {target}");
+
+            var assetBundleDirectory = AssetBundleDirectory + "/" + target.ToString();
             if (!Directory.Exists(assetBundleDirectory))
             {
                 Directory.CreateDirectory(assetBundleDirectory);
             }
 
-            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None,
-                BuildTarget.WebGL);
+            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, target);
+
+            Debug.LogWarning($"AssetBundles built to dir: {assetBundleDirectory}");
+        }
+
+        [MenuItem("Assets/Build AssetBundles/Windows")]
+        private static void BuildAssetBundlesWindows()
+        {
+            BuildAssetBundle(BuildTarget.StandaloneWindows);
+        }
+
+        [MenuItem("Assets/Build AssetBundles/WebGL")]
+        private static void BuildAssetBundlesWebGl()
+        {
+            BuildAssetBundle(BuildTarget.WebGL);
+        }
+
+        [MenuItem("Assets/Build AssetBundles/All")]
+        private static void BuildAssetBundlesAll()
+        {
+            foreach (BuildTarget target in SupportedBuildTargets)
+            {
+                BuildAssetBundle(target);
+            }
         }
     }
 }
